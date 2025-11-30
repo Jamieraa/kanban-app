f6f2309f995d00e49cc346e97d424d1fa9bcc1d9
Jamiera <jamiera@macbookpro.mynetworksettings.com>
2025-11-30 01:19:22 -0500

    FIX: Updated boards/apps.py name attribute to full path backend.boards.

--- backend/boards/apps.py
+++ backend/boards/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 class BoardsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'  # default primary key field type
-    name = 'boards'  # name of the app
+    name = 'backend.boards'  # name of the app
