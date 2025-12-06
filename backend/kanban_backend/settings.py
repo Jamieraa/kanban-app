3d8811019bbc212b97eafaf10bed14975d88bbb9
Jamiera <jamiera@Jamieras-MacBook-Pro.local>
2025-12-06 17:10:20 -0500

    CONFIG: Switched database connection to Supabase PostgreSQL

--- backend/kanban_backend/settings.py
+++ backend/kanban_backend/settings.py
@@ -67,8 +67,12 @@ WSGI_APPLICATION = "backend.kanban_backend.wsgi.application"
 # ---------------------------------------------------------
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",  
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "XG56ckYOHiHQWuKQ",
+        "HOST": "db.mklnflltxfamwnpcdfut.supabase.co",
+        "PORT": "5432",
     }
 }
 
